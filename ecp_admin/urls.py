f6282b7a41a321b8f29568e8e53e2ed1363e5fa9
inayat2291@gmail.com <inayat2291@gmail.com>
2026-08-10 20:52:30 +0500

    urls.py

--- ecp_admin/urls.py
+++ ecp_admin/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("", views.ecp_login, name="ecp_login"),
     path("dashboard/", views.ecp_dashboard, name="ecp_dashboard"),
     path("election-creation/", views.election_creation, name="election_creation"),
-    # path("upload-election-data/", views.upload_election_data, name="upload_election_data"),
 ]

d262027ebd2eb30571c3f706d0398cb283fb1067
inayat2291@gmail.com <inayat2291@gmail.com>
2026-08-11 16:55:27 +0500

    basic update

--- ecp_admin/urls.py
+++ ecp_admin/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.ecp_dashboard, name="ecp_dashboard"),
     path("login/", views.ecp_login, name="ecp_login"),
-    path("form/", views.ecp_election_creation_form, name="ecp_election_creation_form"),
+    path("election/", views.ecp_election_creation_form, name="ecp_election_creation_form"),
     path("upload/", views.ecp_election_upload, name="ecp_election_upload"),
-    path("election/", views.ecp_election_creation, name="ecp_election_creation"),
     path("logout/", views.ecp_logout, name="ecp_logout"),
 ]
